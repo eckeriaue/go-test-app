f24a549bcae78307d070809ca7360fae4d25668b
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-10-01 12:45:49 +0300

    add ternar

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+
 	var a int8 = 2
 	var b int64 = int64(a)
 	fmt.Print(b)
@@ -11,5 +12,12 @@ func main() {
 
 	fmt.Scan(&c)
 	fmt.Println(fmt.Sprint(b) + fmt.Sprint(c))
+
+	num := 4
+	if num > 3 {
+		fmt.Println("num is greater of 3")
+	} else {
+		fmt.Println("num is not greater of 3")
+	}
 
 }
